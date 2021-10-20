a27139969fd2a0704bb0fe5e5f59b2f168743d58
JMerchan <jimmymerchan1603@gmail.com>
2021-10-19 21:53:20 -0500

    se agrega la creación de un array de objetos

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 
 var misCuadernos = [];
-var CuadernoUno = ["Tipo Agenda", "Paisajes", "Cuadriculado", "De dibujo"];
-var CuadernoDos = ["Argollado", "Tipo Folder" , "Rayado" , "Ferrrocarril"];
+var listaUno = ["Tipo Agenda", "Paisajes", "Cuadriculado", "De dibujo"];
+var listaDos = ["Argollado", "Tipo Folder" , "Rayado" , "Ferrrocarril"];
 
-function imprimirLista(CuadernoUno, CuadernoDos){
+function imprimirLista(listaUno, listaDos){
     //metodo para unir los 2 arrays
-    misCuadernos = CuadernoUno.concat(CuadernoDos);
+    misCuadernos = listaUno.concat(listaDos);
     console.log(misCuadernos);
     // array como cadena string separados por caracteres
     console.log(misCuadernos.join("---"));
 }
-imprimirLista(CuadernoUno, CuadernoDos)
+imprimirLista(listaUno, listaDos)
+
+
+//se crea una lista de objetos pensando en el proyecto
+
+const listaUtiles = [];
+class Cuadernos{
+    nombre;
+    precio;
+    stock;
+    constructor (precio, nombre, stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+
+let CuadernoUno = new Cuadernos (17000, "Agenda", 15);
+let CuadernoDos = new Cuadernos (4800, "Stickers",6);
+let CuadernoTres = new Cuadernos (10500, "5 Materias", 21);
+let CuadernoCuatro = new Cuadernos (2800, "Argollado", 7);
+
+//Se agregan los objetos a listaUtiles
+listaUtiles.push (CuadernoUno);
+listaUtiles.push (CuadernoDos);
+listaUtiles.push (CuadernoTres);
+listaUtiles.push (CuadernoCuatro);
+
+// Para mostrar los objetos de la listaUtiles;
+for (let i=0; i<listaUtiles.length;i++){
+    console.log(listaUtiles[i])
+}
